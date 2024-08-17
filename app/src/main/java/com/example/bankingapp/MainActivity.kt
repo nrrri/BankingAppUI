8fa84a8e2098532920b0c71caccf0b3f7a19c9eb
Ferin.Chow <nrrri.cho@gmail.com>
2024-08-16 21:25:03 -0400

    added Currencies section

--- app/src/main/java/com/example/bankingapp/MainActivity.kt
+++ app/src/main/java/com/example/bankingapp/MainActivity.kt
@@ -12,10 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +33,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//HhomeScreen()
                     HomeScreen()
                 }
             }
@@ -71,7 +68,7 @@ fun HomeScreen() {
             CardsSection()
             Spacer(modifier = Modifier.height(16.dp))
                     FinanceSection()
-            //        CurrenciesSection()
+                    CurrenciesSection()
         }
 
     }
